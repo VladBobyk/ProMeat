c08c0ecb289595c7e3cae99307d2154b0abfb079
VladBobyk <35531964+VladBobyk@users.noreply.github.com>
2024-01-25 17:25:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,8 @@ document.addEventListener('DOMContentLoaded', function () {
         var calculatedPrice = totalPrice * quantityInputCard.value;
 
         // Оновлення вмісту елемента з ціною та атрибуту price
-        priceElement.textContent = (calculatedPrice > 0) ? calculatedPrice.toFixed(2).split('.')[1] === '00' ? calculatedPrice.toFixed(0) + ' ₴' : calculatedPrice.toFixed(2) + ' ₴' : '0 ₴';
+        var formattedPrice = calculatedPrice.toFixed(2);
+        priceElement.textContent = (calculatedPrice > 0) ? (formattedPrice.endsWith('.00') ? formattedPrice.split('.')[0] : formattedPrice) + ' ₴' : '0 ₴';
         priceElement.setAttribute('price', calculatedPrice.toFixed(2));
     }
 });
