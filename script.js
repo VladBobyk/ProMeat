d57c3eaf74a858ea8cf8e2af85e3564fab6c2215
VladBobyk <35531964+VladBobyk@users.noreply.github.com>
2024-02-02 13:44:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function updateCartNumber() {
 function saveCart() {
     var cartItems = Array.from(cartItemsContainer.children()).map(item => {
         var initialPricePerUnit = parseFloat($(item).data('initial-price')) || 0;
-        var quantity = parseInt($(item).find('.quantity_cart').val(), 100) || 1;
+        var quantity = parseInt($(item).find('.quantity_cart').val(), 10) || 1;
         var totalPrice = initialPricePerUnit * quantity;
 
         if (!isNaN(totalPrice)) {
